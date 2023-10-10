aca7399a9300bc275cb007a49074038ccce1dfb3
mzaini30 <muhzaini30@gmail.com>
2023-10-10 15:05:38 +0800

    sudah

---
+++ src/pages/aplikasi/notes/notes.svelte
@@ -0,0 +1,13 @@
+<script>
+  let isinya = "";
+  if (localStorage.isinya) {
+    isinya = localStorage.isinya;
+  }
+  $: localStorage.isinya = isinya;
+</script>
+
+<textarea
+  class="textarea textarea-bordered h-[calc(100vh-130px)]"
+  placeholder=""
+  bind:value={isinya}
+/>
